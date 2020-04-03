c45bfb26708f47ff5c1023a54bb2c84fd98b8cdf
luk <schwarzblade@gamil.com>
2020-04-03 15:16:14 +0200

    business logic

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -4,11 +4,12 @@ class MemeGenerator extends React.Component {
 	constructor(){
 		super();
 		this.state = {
-			topText: " ",
-			bottomText: " ",
+			topText: "",
+			bottomText: "",
 			randomImgUrl: "http://i.imgflip.com/1bij.jpg",
 			allMemeImgs: []
 		}
+		this.onChange = this.onChange.bind(this)
 	}
 
 
@@ -22,14 +23,28 @@ class MemeGenerator extends React.Component {
 			})
 	}
 
+	onChange(event) {
+		const {name,value} = event.target
+		this.setState({
+			[name]: value
+		})
+	}
+
 
 	render(){
 		return(
 			<div>
 				<form className="meme-form">
+					<input type="text" name="topText"  placeholder="top text" value={this.state.topText} onChange={this.onChange} />
+					<input type="text" name="bottomText"  placeholder="bottom text" value={this.state.bottomText} onChange={this.onChange} />
 
 					<button>Gen</button>
 				</form>
+				<div className="meme">
+					<img src={this.state.randomImgUrl} alt=""/>
+					<h2 className="top">{this.state.topText}</h2>
+					<h2 className="bottom">{this.state.bottomText}</h2>
+				</div>
 			</div>
 		)
 	}
